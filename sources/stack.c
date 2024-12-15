9f296e7bbd063b4418d4b18c444262d70391ba9e
Hamzah <hamzahtaimour@gmail.com>
2024-12-15 14:34:41 +0300

    Radix sort try

--- sources/stack.c
+++ sources/stack.c
@@ -6,7 +6,7 @@
 /*   By: hassende <hassende@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/28 21:34:31 by hassende          #+#    #+#             */
-/*   Updated: 2024/12/09 14:14:23 by hassende         ###   ########.fr       */
+/*   Updated: 2024/12/15 14:28:51 by hassende         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,10 +30,71 @@ static void	short_sort(t_stack **stack_a)
 	}
 }
 
+int	ft_stack_max(t_stack **stack)
+{
+	int		max;
+	t_stack	*tmp;
+
+	max = INT_MIN;
+	tmp = *stack;
+	while (tmp)
+	{
+		if (tmp->value > max)
+			max = tmp->value;
+		tmp = tmp->next;
+	}
+	return (max);
+}
+
+int	ft_stack_min(t_stack **stack)
+{
+	int		min;
+	t_stack	*tmp;
+
+	min = INT_MAX;
+	tmp = *stack;
+	while (tmp)
+	{
+		if (tmp->value < min)
+			min = tmp->value;
+		tmp = tmp->next;
+	}
+	return (min);
+}
+
+void	radix_sort(t_stack **stack_a, t_stack **stack_b)
+{
+	int		i;
+	int		j;
+	int		max;
+	int		min;
+
+	i = 0;
+	max = ft_stack_max(stack_a);
+	min = ft_stack_min(stack_a);
+	while (i < 32)
+	{
+		j = 0;
+		while (j < ft_stack_size(stack_a))
+		{
+			if (((*stack_a)->value >> i) & 1)
+				ra(stack_a);
+			else
+				push_b(stack_a, stack_b);
+			j++;
+		}
+		while (ft_stack_size(stack_b))
+			push_a(stack_a, stack_b);
+		i++;
+	}
+}
+
 void	algrothim(t_stack **stack_a, t_stack **stack_b)
 {
 	if (ft_stack_size(stack_a) <= 3)
 		short_sort(stack_a);
+	if (ft_stack_size(stack_a) > 5)
+		radix_sort(stack_a, stack_b);
 }
 
 void	init_stack(t_stack **stack_a, char **args)
